1308d62dd71f3bc3fa75b14582f7c517dbbdfe3f
Julian Aleksandrowicz <62268641+ju-aleksandrowicz@users.noreply.github.com>
2022-10-30 18:37:04 -0300

    Se crea la clase reserva

--- ProyectoHotel/ProyectoHotel.Entidades/Reserva.cs
+++ ProyectoHotel/ProyectoHotel.Entidades/Reserva.cs
@@ -8,5 +8,23 @@ namespace ProyectoHotel.Entidades
 {
     internal class Reserva
     {
+        public Reserva(int id, int idCliente, int idHabitacion, int cantidadHuespedes, DateTime fechaIngreso, DateTime fechaEgreso)
+        {
+            _id = id;
+            _idCliente = idCliente;
+            _idHabitacion = idHabitacion;
+            _cantidadHuespedes = cantidadHuespedes;
+            _fechaIngreso = fechaIngreso;
+            _fechaEgreso = fechaEgreso;
+        }
+
+        private int _id;
+        private int _idCliente;
+        private int _idHabitacion;
+        private int _cantidadHuespedes;
+        private DateTime _fechaIngreso;
+        private DateTime _fechaEgreso;
+
+
     }
 }
