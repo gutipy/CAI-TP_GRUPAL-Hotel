965bdbe0c0a30639c4667d238bc5fdbaf0f2076c
Julian Aleksandrowicz <62268641+ju-aleksandrowicz@users.noreply.github.com>
2022-11-01 00:18:26 -0300

    Se crea la clase cliente

--- ProyectoHotel/ProyectoHotel.Entidades/Cliente.cs
+++ ProyectoHotel/ProyectoHotel.Entidades/Cliente.cs
@@ -8,5 +8,21 @@ namespace ProyectoHotel.Entidades
 {
     internal class Cliente
     {
+        public Cliente(int id, DateTime fechaAlta, bool activo)
+        {
+            _id = id;
+            _fechaAlta = fechaAlta;
+            _activo = activo;
+        }
+
+        private int _id;
+        private DateTime _fechaAlta;
+        private bool _activo;
+
+        public int Id { get { return _id; } }
+
+        public DateTime FechaAlta { get { return _fechaAlta; } }
+
+        public bool Activo { get { return _activo; } }
     }
 }
