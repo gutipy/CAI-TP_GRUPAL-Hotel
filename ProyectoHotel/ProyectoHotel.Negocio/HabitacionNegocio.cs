a50a4a79a7b8e1707bbf67fa4acbcdc0b9a062d1
tomashardoy98 <111935492+tomashardoy98@users.noreply.github.com>
2022-11-14 23:55:02 -0300

    Add files via upload

---
+++ ProyectoHotel/ProyectoHotel.Negocio/HabitacionNegocio.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProyectoHotel.Datos;
+using ProyectoHotel.Entidades;
+using ProyectoHotel.Negocio.Excepciones;
+
+namespace ProyectoHotel.Negocio
+{
+    public class HabitacionNegocio
+    {
+        //Atributos
+        private HabitacionDatos _habitacionDatos;
+
+        //Constructores
+        public HabitacionNegocio()
+        {
+            _habitacionDatos = new HabitacionDatos();
+        }
+
+        //Funciones-Métodos
+        public List<Habitacion> GetLista(string idHotel) //Función para listar a todas las habitaciones existentes por hotel
+        {
+            //Declaración de variables
+            List<Habitacion> _totalHabitaciones = new List<Habitacion>();
+
+            _totalHabitaciones = _habitacionDatos.TraerPorHotel(idHotel); //Guardo en la lista '_totalHabitaciones' los datos de todas las habitaciones de un hotel específico que me trae la capa de Acceso a datos
+
+            return _totalHabitaciones; //Devuelvo la lista con todas las habitaciones por hotel a la capa de presentación
+        }
+
+        public void AgregarHabitacion(Habitacion nuevaHabitacion)
+        {
+            //Declaración de variables
+            List<Habitacion> _totalHabitaciones = new List<Habitacion>();
+
+            _totalHabitaciones = _habitacionDatos.TraerPorHotel(nuevaHabitacion.IdHotel.ToString()); //Traigo la lista total de habitaciones por hotel de la capa de datos y la guardo en la lista '_totalHabitaciones'
+
+            if (nuevaHabitacion == null) //Si el objeto que llega por parámetro es nulo se lo comunico al usuario mediante una excepción custom
+            {
+                throw new HabitacionInvalidaException();
+            }
+
+            else if (_totalHabitaciones.Find(reg => reg.Id == nuevaHabitacion.Id) != null) //Si dentro de la lista de habitaciones encuentro alguna que tenga el mismo id que la nueva habitación se lo comunico al usuario mediante una excepción custom
+            {
+                throw new ObjetoExistenteException("Habitación", nuevaHabitacion.Id);
+            }
+
+            else if (_totalHabitaciones.Count > 100) //Regla de negocio que implica que si un hotel posee más de 100 habitaciones como maximo entonces le muestro una excepcion al user
+            {
+                throw new CantidadHabitacionesPorHotelException();
+            }
+            else
+            {
+                nuevaHabitacion.Categoria = ValidarCategoria(nuevaHabitacion.Categoria); //Valido y transformo la categoría recibida en número en la que corresponda (1 - Single, 2 - Double, etc.)
+
+                TransactionResult transaction = _habitacionDatos.Insertar(nuevaHabitacion); //Agrego la habitación al repositorio de datos
+
+                if (!transaction.IsOk) //Si la transacción no se pudo completar le comunico el error al usuario mediante una excepción
+                {
+                    throw new Exception(transaction.Error);
+                }
+            }
+        }
+
+        public string ValidarCategoria(string categoria)
+        {
+            if (categoria == "1")
+            {
+                return "Single";
+            }
+
+            else if (categoria == "2")
+            {
+                return "Double";
+            }
+
+            else if (categoria == "3")
+            {
+                return "Quadruple";
+            }
+
+            else if (categoria == "4")
+            {
+                return "Junior Suite";
+            }
+
+            else if (categoria == "5")
+            {
+                return "Suite";
+            }
+
+            else
+            {
+                return "Grand Suite";
+            }
+        }
+    }
+}
---
+++ ProyectoHotel/ProyectoHotel.Negocio/HotelNegocio.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProyectoHotel.Datos;
+using ProyectoHotel.Entidades;
+using ProyectoHotel.Negocio.Excepciones;
+
+namespace ProyectoHotel.Negocio
+{
+    public class HotelNegocio
+    {
+        //Atributos
+        private HotelDatos _hotelDatos;
+
+        //Constructores
+        public HotelNegocio()
+        {
+            _hotelDatos = new HotelDatos();
+        }
+
+        //Funciones-Métodos
+        public List<Hotel> GetLista() //Función para listar a todos los hoteles por usuario
+        {
+            //Declaración de variables
+            List<Hotel> _totalHoteles = new List<Hotel>();
+
+            _totalHoteles = _hotelDatos.Traer(888086); //Guardo en la lista '_totalHoteles' los datos de todos los hoteles por usuario que me trae la capa de Datos
+
+            return _totalHoteles; //Devuelvo la lista con todos los hoteles por usuario a la capa de presentación
+        }
+
+        public void AgregarHotel(Hotel nuevoHotel)
+        {
+            //Declaración de variables
+            List<Hotel> _totalHoteles = new List<Hotel>();
+
+            _totalHoteles = _hotelDatos.Traer(888086); //Guardo en la lista '_totalHoteles' los datos de todos los hoteles por usuario que me trae la capa de Datos
+
+            if (nuevoHotel == null) //Si el objeto que llega por parámetro es nulo se lo comunico al usuario mediante una excepción custom
+            {
+                throw new ObjetoInvalidoException("Hotel");
+            }
+
+            else if (_totalHoteles.Find(reg => reg.Id == nuevoHotel.Id) != null) //Si dentro de la lista de hoteles encuentro alguno que tenga el mismo id que el nuevo hotel se lo comunico al usuario mediante una excepción custom
+            {
+                throw new ObjetoExistenteException("Hotel", nuevoHotel.Id);
+            }
+
+            else if (nuevoHotel.Estrellas < 1 || nuevoHotel.Estrellas > 5) //Regla de negocio que implica que si un hotel posee menos de 1 estrella o más de 5 entonces le muestro una excepcion al user
+            {
+                throw new CantidadEstrellasException();
+            }
+
+            else
+            {
+                nuevoHotel.Amenities = ValidarAmenities(nuevoHotel.Estrellas, nuevoHotel.Amenities); //Valido que la configuración de las amenities para los nuevos hoteles respete la regla de negocio indicada en la firma del método 'ValidarAmenities'
+
+                TransactionResult transaction = _hotelDatos.Insertar(nuevoHotel); //Agrego el hotel al repositorio de datos
+
+                if (!transaction.IsOk) //Si la transacción no se pudo completar le comunico el error al usuario mediante una excepción
+                {
+                    throw new Exception(transaction.Error);
+                }
+            }
+        }
+
+        public bool ValidarAmenities(int estrellas, bool amenities) //Regla de negocio que implica que los hoteles de 2 o menos estrellas NO pueden tener amenities y los de 3 o más estrellas DEBEN tener amenities sino le comunico al usuario mediante excepcion custom
+        {
+            if (estrellas <= 2)
+            {
+                if (amenities == true)
+                {
+                    throw new AmenitiesInvalidasException();
+                }
+
+                else
+                {
+                    return false;
+                }
+
+            }
+
+            else
+            {
+                if (amenities == false)
+                {
+                    throw new AmenitiesInvalidasException();
+                }
+
+                else
+                {
+                    return true;
+                }
+            }
+        }
+    }
+}
